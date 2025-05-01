3fa9e31069b1ea8dca83a29a2ad5cf730080c4e2
Mahmoud Atoot <mahmoudatoot.99@gmail.com>
2025-05-01 15:30:14 +0300

    ticket manager

--- TicketFlow/app/models.py
+++ TicketFlow/app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class TicketManager(models.Manager):
+    def validate_ticket(self, postData):
+        errors = {}
+        if len(postData['title']) < 2:
+            errors['title'] = "Title must be at least 2 characters"
+        if len(postData['description']) < 15:
+            errors['description'] = "Description must be at least 15 characters"
+        return errors
+ 
