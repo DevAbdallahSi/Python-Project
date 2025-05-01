bad7c91976cf43952a5c14577ea8361268cec85e
BasheerShalabi <shalabibasheer@gmail.com>
2025-05-01 15:30:03 +0300

    Ticket model , Status model

--- TicketFlow/app/models.py
+++ TicketFlow/app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Status(models.Model):
+    name = models.CharField(max_length=40)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Ticket(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    priority = models.CharField(max_length=20)
+    status = models.ForeignKey(Status,related_name="status_tickets")
+    issuer = models.ForeignKey(User,related_name="user_tickets")
+    assigned_to = models.ForeignKey(Department,related_name="dp_tickets")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
